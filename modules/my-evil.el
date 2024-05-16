300330e7e9e5c3ad384e9dcd5f16e8f63299e68e
Belly Fat <hmshen@Haomings-MacBook-Pro.local>
2024-05-16 16:19:20 -0700

    remove redun

--- modules/my-evil.el
+++
@@ -1,8 +0,0 @@
-;;; my-evil.el --- evil customizations for Emacs  -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Emacs Startup File --- evil for Emacs
-;;; Package --- Summary
-;;; Code:
-
-(provide 'my-evil)
-;;; my-evil ends here
